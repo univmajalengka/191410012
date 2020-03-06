820915fd1c441189f38b5467e4bc29d10f2bd85f
rifanurfalah31 <rifa.kbpc@gmail.com>
2020-03-06 18:15:07 +0700

    Tugas dua Struktur data I

---
+++ Struktur Data I/Tugas II Array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int nilai[25];
+int a;
+
+int main()
+{
+	cout<<"Masukkan jumlah nilai yang diinginkan 1-25 : ";
+	cin>>a;
+	cout<<endl;
+	for(int b=1;b<=a;b++)
+	{
+		cout<<"Masukkan nilai yang ke : "<<b<< " : ";
+		cin >>nilai[b];
+	}
+	cout<<endl;
+	cout<<"Nilai yang telah dimasukkan : "<<endl;
+	for(int b=1;b<=a;b++)
+	{
+		cout<<"nilai ke-" <<b<< "= "<<nilai[b]<<endl;
+	}
+	return 0;
+}
