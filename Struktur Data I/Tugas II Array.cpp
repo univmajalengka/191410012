752bb1027e3fbbf5701de9c711275cb1143fbc21
Rifa Nurfalah <56184050+rifanurfalah31@users.noreply.github.com>
2020-03-06 19:39:30 +0700

    Update Tugas II Array.cpp

--- Struktur Data I/Tugas II Array.cpp
+++ Struktur Data I/Tugas II Array.cpp
@@ -6,17 +6,17 @@ int a;
 
 int main()
 {
-	cout<<"Masukkan jumlah nilai yang diinginkan 1-25 : ";
+	cout<<"Masukkan jumlah nilai yang diinginkan maksimal 25 : ";
 	cin>>a;
 	cout<<endl;
-	for(int b=1;b<=a;b++)
+	for(int b=0;b<=a;b++)
 	{
 		cout<<"Masukkan nilai yang ke : "<<b<< " : ";
 		cin >>nilai[b];
 	}
 	cout<<endl;
 	cout<<"Nilai yang telah dimasukkan : "<<endl;
-	for(int b=1;b<=a;b++)
+	for(int b=0;b<=a;b++)
 	{
 		cout<<"nilai ke-" <<b<< "= "<<nilai[b]<<endl;
 	}
