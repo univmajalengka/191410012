7e876af9e6376c78f035bc2692fae6c4d8e817e8
Rifa Nurfalah <56184050+rifanurfalah31@users.noreply.github.com>
2020-05-14 17:11:34 +0700

    Update bintang terbalik.cpp

--- Struktur Data I/bintang terbalik.cpp
+++ Struktur Data I/bintang terbalik.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-for(int a=5;a>=1;a--){
+for(int a=6;a>=1;a--){
 	for( int b=1;b<a;b++){
 		cout << " ";
 			}
-	for(int c=5;c>=a;c--){
+	for(int c=6;c>=a;c--){
 		cout << "*";
 	}
 	cout<<endl;
